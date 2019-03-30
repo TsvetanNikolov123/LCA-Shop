fd235876ec901b831eb57d363c49d232a062dda6
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-03-30 19:46:26 +0200

    Add properties in application.properties file

---
+++ src/main/java/org/softuni/lcashop/LcashopApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.lcashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcashopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/softuni/lcashop/LcashopApplicationTests.java
@@ -0,0 +1,16 @@
+package org.softuni.lcashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LcashopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
