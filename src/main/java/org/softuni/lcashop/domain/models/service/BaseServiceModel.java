7cd1b79a3d83ee29d85e916444e5078bd8d0fc22
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-03-31 20:01:41 +0300

    Made RoleServiceModel, UserServiceModel and abstract class BaseServiceModel

---
+++ src/main/java/org/softuni/lcashop/domain/models/service/BaseServiceModel.java
@@ -0,0 +1,17 @@
+package org.softuni.lcashop.domain.models.service;
+
+public abstract class BaseServiceModel {
+
+    private String id;
+
+    protected BaseServiceModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/org/softuni/lcashop/domain/models/service/RoleServiceModel.java
@@ -0,0 +1,17 @@
+package org.softuni.lcashop.domain.models.service;
+
+public class RoleServiceModel extends BaseServiceModel {
+
+    private String authority;
+
+    public RoleServiceModel() {
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
