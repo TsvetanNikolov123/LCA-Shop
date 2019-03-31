eb082eeb243eb8b63137a4ac82aeed039b237e10
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-03-31 21:27:49 +0300

    Removed method "assignUserRoles" and added two new methods - "findAllRoles" and "findByAuthority".

--- src/main/java/org/softuni/lcashop/service/RoleService.java
+++ src/main/java/org/softuni/lcashop/service/RoleService.java
@@ -1,10 +1,15 @@
 package org.softuni.lcashop.service;
 
+import org.softuni.lcashop.domain.models.service.RoleServiceModel;
 import org.softuni.lcashop.domain.models.service.UserServiceModel;
+
+import java.util.Set;
 
 public interface RoleService {
 
     void seedRolesInDb();
 
-    void assignUserRoles(UserServiceModel userServiceModel);
+    Set<RoleServiceModel> findAllRoles();
+
+    RoleServiceModel findByAuthority(String authority);
 }
---
+++ src/main/java/org/softuni/lcashop/service/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package org.softuni.lcashop.service;
+
+import org.modelmapper.ModelMapper;
+import org.softuni.lcashop.domain.entities.User;
+import org.softuni.lcashop.domain.models.service.UserServiceModel;
+import org.softuni.lcashop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserServiceImpl(
+            UserRepository userRepository,
+            RoleService roleService,
+            ModelMapper modelMapper,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleService = roleService;
+        this.modelMapper = modelMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public UserServiceModel registerUser(UserServiceModel userServiceModel) {
+        this.roleService.seedRolesInDb();
+        if (this.userRepository.count() == 0) {
+            userServiceModel.setAuthorities(this.roleService.findAllRoles());
+        } else {
+            userServiceModel.setAuthorities(new HashSet<>());
+            userServiceModel.getAuthorities().add(this.roleService.findByAuthority("ROLE_USER"));
+        }
+        User user = this.modelMapper.map(userServiceModel, User.class);
+        user.setPassword(this.bCryptPasswordEncoder.encode(userServiceModel.getPassword()));
+
+        return this.modelMapper.map(this.userRepository.saveAndFlush(user), UserServiceModel.class);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+    }
+}
