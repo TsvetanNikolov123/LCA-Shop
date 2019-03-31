b78449a9c6000d913f182ae47060fbb89384f1c4
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-03-31 19:07:01 +0300

    Added RoleRepository and UserRepository interfaces.

---
+++ src/main/java/org/softuni/lcashop/domain/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.softuni.lcashop.domain.repository;
+
+import org.softuni.lcashop.domain.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+    Role findByAuthority(String authority);
+}
