8b9ab44f421d9069a38df175d20e71acb8553049
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-02 11:19:32 +0300

    Added UserService and ModelMapper as final fields, and @Autowired them in constructor. Added registerConfirm Post method and login Get method;

--- src/main/java/org/softuni/lcashop/web/controllers/UserController.java
+++ src/main/java/org/softuni/lcashop/web/controllers/UserController.java
@@ -1,8 +1,15 @@
 package org.softuni.lcashop.web.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.softuni.lcashop.domain.models.binding.UserRegisterBindingModel;
+import org.softuni.lcashop.domain.models.service.UserServiceModel;
+import org.softuni.lcashop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,9 +17,36 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/users")
 public class UserController extends BaseController {
 
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping("/register")
     @PreAuthorize("isAnonymous()")
     public ModelAndView register(){
         return super.view("register");
+    }
+
+    @PostMapping
+    @PreAuthorize("isAnonymous()")
+    public ModelAndView registerConfirm(@ModelAttribute UserRegisterBindingModel userRegisterBindingModel){
+        if (!userRegisterBindingModel.getPassword().equals(userRegisterBindingModel.getConfirmPassword())){
+            return super.view("register");
+        }
+
+        this.userService.registerUser(this.modelMapper.map(userRegisterBindingModel, UserServiceModel.class));
+
+        return redirect("/login");
+    }
+
+    @GetMapping("/login")
+    @PreAuthorize("isAnonymous()")
+    public ModelAndView login(){
+        return super.view("login");
     }
 }
