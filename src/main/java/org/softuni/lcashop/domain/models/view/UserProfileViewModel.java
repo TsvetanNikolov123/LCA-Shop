75b099674528cecc02fe08bd055218d38be79621
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-02 13:23:13 +0300

    Added UserProfileViewModel with attributes for id, username and email . Added empty constructor and getters and setters

---
+++ src/main/java/org/softuni/lcashop/domain/models/view/UserProfileViewModel.java
@@ -0,0 +1,35 @@
+package org.softuni.lcashop.domain.models.view;
+
+public class UserProfileViewModel {
+
+    private String id;
+    private String username;
+    private String email;
+
+    public UserProfileViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
