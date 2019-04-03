666f3b7d34319accd137105c9917196d271f4d3b
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-03 16:05:16 +0300

    added UserEditBindingModel

---
+++ src/main/java/org/softuni/lcashop/domain/models/binding/UserEditBindingModel.java
@@ -0,0 +1,44 @@
+package org.softuni.lcashop.domain.models.binding;
+
+public class UserEditBindingModel {
+
+    private String username;
+    private String oldPassword;
+    private String password;
+    private String confirmPassword;
+
+    public UserEditBindingModel() {
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOldPassword() {
+        return this.oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return this.confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
