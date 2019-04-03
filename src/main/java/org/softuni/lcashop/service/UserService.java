013fa07551b6d4b5d7ede7117cedec67e3b77202
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-03 23:10:26 +0300

    added method which finds all users

--- src/main/java/org/softuni/lcashop/service/UserService.java
+++ src/main/java/org/softuni/lcashop/service/UserService.java
@@ -3,6 +3,8 @@ package org.softuni.lcashop.service;
 import org.softuni.lcashop.domain.models.service.UserServiceModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
 
     UserServiceModel registerUser(UserServiceModel userServiceModel);
@@ -10,4 +12,6 @@ public interface UserService extends UserDetailsService {
     UserServiceModel findUserByUserName(String username);
 
     UserServiceModel editUserProfile(UserServiceModel userServiceModel, String oldPassword);
+
+    List<UserDetailsService> findAllUsers();
 }
