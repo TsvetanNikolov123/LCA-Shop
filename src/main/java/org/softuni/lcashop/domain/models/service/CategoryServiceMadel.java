569be923e68f151ad86d09ec7caaa5225b005715
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-04 16:55:33 +0300

    Add Class CategoryServiceModel

---
+++ src/main/java/org/softuni/lcashop/domain/models/service/CategoryServiceMadel.java
@@ -0,0 +1,17 @@
+package org.softuni.lcashop.domain.models.service;
+
+public class CategoryServiceMadel extends BaseServiceModel {
+
+    private String name;
+
+    public CategoryServiceMadel() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
