ed4943e4aa5c87612b0c57e42392e0b59acdedd1
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-04 20:07:28 +0300

    Add Class CategoryViewModel with 2 fields -> id, name

---
+++ src/main/java/org/softuni/lcashop/domain/models/view/CategoryViewModel.java
@@ -0,0 +1,26 @@
+package org.softuni.lcashop.domain.models.view;
+
+public class CategoryViewModel {
+
+    private String id;
+    private String name;
+
+    public CategoryViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
