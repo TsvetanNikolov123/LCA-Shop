22cbae4b7d490b4f57e1f02be3dc892509fb118e
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-04 20:11:27 +0300

    Added new method findAllCategories

--- src/main/java/org/softuni/lcashop/service/CategoryService.java
+++ src/main/java/org/softuni/lcashop/service/CategoryService.java
@@ -2,7 +2,11 @@ package org.softuni.lcashop.service;
 
 import org.softuni.lcashop.domain.models.service.CategoryServiceModel;
 
+import java.util.List;
+
 public interface CategoryService {
 
     CategoryServiceModel addCategory(CategoryServiceModel categoryServiceModel);
+
+    List<CategoryServiceModel> findAllCategories();
 }
