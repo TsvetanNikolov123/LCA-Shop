6130dcb39360b57b7294d27cc3b519f016d04b7e
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-05 02:37:39 +0300

    fix -> Product class was not extend BaseEntity

--- src/main/java/org/softuni/lcashop/domain/entities/Product.java
+++ src/main/java/org/softuni/lcashop/domain/entities/Product.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "products")
-public class Product {
+public class Product extends BaseEntity {
 
     private String name;
     private String description;
