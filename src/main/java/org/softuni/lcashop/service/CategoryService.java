677004c2bc640e84dd07e777192b3cfc5f780053
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-04 17:02:39 +0300

    Add Class CategoryService with one method "addCategory"

---
+++ src/main/java/org/softuni/lcashop/service/CategoryService.java
@@ -0,0 +1,8 @@
+package org.softuni.lcashop.service;
+
+import org.softuni.lcashop.domain.models.service.CategoryServiceModel;
+
+public interface CategoryService {
+
+    CategoryServiceModel addCategory(CategoryServiceModel categoryServiceModel);
+}
