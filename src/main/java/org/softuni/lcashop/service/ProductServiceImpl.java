3b0536e158628985100283c8de689ad3f646bb3f
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-05 16:27:26 +0300

    Implement method findAllProducts;

--- src/main/java/org/softuni/lcashop/service/ProductServiceImpl.java
+++ src/main/java/org/softuni/lcashop/service/ProductServiceImpl.java
@@ -7,6 +7,9 @@ import org.softuni.lcashop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProductServiceImpl implements ProductService {
 
@@ -23,5 +26,14 @@ public class ProductServiceImpl implements ProductService {
     public ProductServiceModel addProduct(ProductServiceModel productServiceModel) {
         Product product = this.modelMapper.map(productServiceModel, Product.class);
         return this.modelMapper.map(this.productRepository.saveAndFlush(product), ProductServiceModel.class);
+    }
+
+    @Override
+    public List<ProductServiceModel> findAllProducts() {
+        return this.productRepository
+                .findAll()
+                .stream()
+                .map(product -> this.modelMapper.map(product, ProductServiceModel.class))
+                .collect(Collectors.toList());
     }
 }
