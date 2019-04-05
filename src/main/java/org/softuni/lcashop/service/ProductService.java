4868e6dd6ed7558ad4a05260a134ba55da619ecd
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-05 16:23:05 +0300

    Added new method which findAllProducts and returns List<ProductServiceModel>

--- src/main/java/org/softuni/lcashop/service/ProductService.java
+++ src/main/java/org/softuni/lcashop/service/ProductService.java
@@ -2,7 +2,11 @@ package org.softuni.lcashop.service;
 
 import org.softuni.lcashop.domain.models.service.ProductServiceModel;
 
+import java.util.List;
+
 public interface ProductService {
 
     ProductServiceModel addProduct(ProductServiceModel productServiceModel);
+
+    List<ProductServiceModel> findAllProducts();
 }
