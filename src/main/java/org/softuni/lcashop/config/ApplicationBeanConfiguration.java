0e6054ab8a355edfb310347e31e7247c6ba1054e
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-07 22:00:15 +0300

    change ModelMapper bean to custom ModelMapper bean

--- src/main/java/org/softuni/lcashop/config/ApplicationBeanConfiguration.java
+++ src/main/java/org/softuni/lcashop/config/ApplicationBeanConfiguration.java
@@ -1,16 +1,23 @@
 package org.softuni.lcashop.config;
 
 import org.modelmapper.ModelMapper;
+import org.softuni.lcashop.mappings.MappingsInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationBeanConfiguration {
+    static ModelMapper modelMapper;
+
+    static {
+        modelMapper = new ModelMapper();
+        MappingsInitializer.initMappings(modelMapper);
+    }
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        return modelMapper;
     }
 
     @Bean
