aa576a43f823821651498ef9c4e86db644a7c5dd
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-07 22:02:55 +0300

    create OrderViewModel class

---
+++ src/main/java/org/softuni/lcashop/domain/models/view/OrderViewModel.java
@@ -0,0 +1,46 @@
+package org.softuni.lcashop.domain.models.view;
+
+import java.math.BigDecimal;
+
+public class OrderViewModel {
+
+    private String imageUrl;
+    private String name;
+    private BigDecimal price;
+    private String customer;
+
+    public OrderViewModel() {
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCustomer() {
+        return this.customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+}
