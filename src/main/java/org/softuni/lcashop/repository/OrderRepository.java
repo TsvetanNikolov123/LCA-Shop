6f299ec69e9897f99fc90699f2b474110ce2fa3a
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-07 15:12:22 +0300

    added OrderRepository class

---
+++ src/main/java/org/softuni/lcashop/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.softuni.lcashop.repository;
+
+import org.softuni.lcashop.domain.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
