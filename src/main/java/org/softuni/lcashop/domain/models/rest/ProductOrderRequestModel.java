8908ac59882ed78ad1adc8364118d8873bdb7be3
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-07 15:13:14 +0300

    added new model class -> "ProductOrderRequestModel"

---
+++ src/main/java/org/softuni/lcashop/domain/models/rest/ProductOrderRequestModel.java
@@ -0,0 +1,14 @@
+package org.softuni.lcashop.domain.models.rest;
+
+public class ProductOrderRequestModel {
+
+    private String id;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
