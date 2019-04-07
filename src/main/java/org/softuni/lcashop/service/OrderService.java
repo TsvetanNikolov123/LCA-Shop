00607059e28b2e15912db69d3b0b384e1ed1905d
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-07 22:05:01 +0300

    create new method findAllOrders in OrderService interface

--- src/main/java/org/softuni/lcashop/service/OrderService.java
+++ src/main/java/org/softuni/lcashop/service/OrderService.java
@@ -1,7 +1,12 @@
 package org.softuni.lcashop.service;
 
+import org.softuni.lcashop.domain.models.service.OrderServiceModel;
+
+import java.util.List;
+
 public interface OrderService {
 
     void createOrder(String productId, String name);
 
+    List<OrderServiceModel> findAllOrders();
 }
