4c2c43444bfb235fcce996bf12bbbf6e9a7479dc
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-08 13:41:00 +0300

    added method findOrdersByUsername in OrderService interface

--- src/main/java/org/softuni/lcashop/service/OrderService.java
+++ src/main/java/org/softuni/lcashop/service/OrderService.java
@@ -2,6 +2,7 @@ package org.softuni.lcashop.service;
 
 import org.softuni.lcashop.domain.models.service.OrderServiceModel;
 
+import java.util.BitSet;
 import java.util.List;
 
 public interface OrderService {
@@ -9,4 +10,6 @@ public interface OrderService {
     void createOrder(String productId, String name);
 
     List<OrderServiceModel> findAllOrders();
+
+    List<OrderServiceModel> findOrdersByCustomer(String username);
 }
