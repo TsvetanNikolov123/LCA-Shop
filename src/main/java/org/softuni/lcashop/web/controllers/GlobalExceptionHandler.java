0dc6c9a3d3076dcf03e4d3266e5c31b1aa387c2f
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-10 01:53:19 +0300

    created GlobalExceptionHandler class

---
+++ src/main/java/org/softuni/lcashop/web/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package org.softuni.lcashop.web.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends BaseController{
+
+}
