42e90535f7d7ddab9329f9d35ac9f037826898a5
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-04-11 17:34:15 +0300

    Added constants in HomeController. Added products to modelAndView

--- src/main/java/org/softuni/lcashop/web/controllers/HomeController.java
+++ src/main/java/org/softuni/lcashop/web/controllers/HomeController.java
@@ -2,7 +2,9 @@ package org.softuni.lcashop.web.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.softuni.lcashop.domain.models.view.CategoryViewModel;
+import org.softuni.lcashop.domain.models.view.ProductAllViewModel;
 import org.softuni.lcashop.service.CategoryService;
+import org.softuni.lcashop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -15,16 +17,28 @@ import java.util.stream.Collectors;
 @Controller
 public class HomeController extends BaseController {
 
+    private static final String CATEGORIES_ATTRIBUTE_NAME = "categories";
+    private static final String PRODUCTS_ATTRIBUTE_NAME = "products";
+    private static final String INDEX_VIEW_NAME = "index";
+    private static final String INDEX_ROUTE = "/";
+    private static final String HOME_ROUTE = "/home";
+    private static final String HOME_VIEW_NAME = "home";
+
     private final CategoryService categoryService;
     private final ModelMapper modelMapper;
+    private final ProductService productService;
 
     @Autowired
-    public HomeController(CategoryService categoryService, ModelMapper modelMapper) {
+    public HomeController(
+            CategoryService categoryService,
+            ModelMapper modelMapper,
+            ProductService productService) {
         this.categoryService = categoryService;
         this.modelMapper = modelMapper;
+        this.productService = productService;
     }
 
-    @GetMapping("/")
+    @GetMapping(INDEX_ROUTE)
     @PreAuthorize("isAnonymous()")
     public ModelAndView index(ModelAndView modelAndView) {
         List<CategoryViewModel> categoryViewModels = this.categoryService
@@ -33,12 +47,18 @@ public class HomeController extends BaseController {
                 .map(category -> this.modelMapper.map(category, CategoryViewModel.class))
                 .collect(Collectors.toList());
 
-        modelAndView.addObject("categories", categoryViewModels);
+        List<ProductAllViewModel> productAllViewModels = this.productService.findAllProducts()
+                .stream()
+                .map(p -> this.modelMapper.map(p, ProductAllViewModel.class))
+                .collect(Collectors.toList());
 
-        return super.view("index", modelAndView);
+        modelAndView.addObject(CATEGORIES_ATTRIBUTE_NAME, categoryViewModels);
+        modelAndView.addObject(PRODUCTS_ATTRIBUTE_NAME, productAllViewModels);
+
+        return super.view(INDEX_VIEW_NAME, modelAndView);
     }
 
-    @GetMapping("/home")
+    @GetMapping(HOME_ROUTE)
     @PreAuthorize("isAuthenticated()")
     public ModelAndView home(ModelAndView modelAndView) {
         List<CategoryViewModel> categoryViewModels = this.categoryService
@@ -47,8 +67,8 @@ public class HomeController extends BaseController {
                 .map(category -> this.modelMapper.map(category, CategoryViewModel.class))
                 .collect(Collectors.toList());
 
-        modelAndView.addObject("categories", categoryViewModels);
+        modelAndView.addObject(CATEGORIES_ATTRIBUTE_NAME, categoryViewModels);
 
-        return super.view("home", modelAndView);
+        return super.view(HOME_VIEW_NAME, modelAndView);
     }
 }
